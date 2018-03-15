63dd265a920cdab161fdaddc9d13b1d1e8166b4f
Armand Rozanski <armand@rozanski.pro>
2018-03-15 20:38:10 +0000

    add initial components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDcEZF7MmMfcJRDf51Je-H3r5LpaaNlpsA';
 
-const APP = () => {
-	return <div>hi</div>;
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		YTSearch({ key: API_KEY, term: 'wallflowers'}, (videos) => {
+			this.setState({ videos });
+		});
+
+		this.state = { videos: [] };
+	}
+
+	render() {
+		return(
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(<APP />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('.container'))
