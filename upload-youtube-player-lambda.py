55b1cce57c1f666c55200950f66af6945f5bb9ac
Armand Rozanski <arozanski@tagworldwide.com>
2018-03-19 15:21:08 +0000

    update deployment script for lambda

--- upload-youtube-player-lambda.py
+++ upload-youtube-player-lambda.py
@@ -7,7 +7,9 @@ import mimetypes
 def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
 
+    topic = sns.Topic('arn:aws:sns:eu-west-2:223963872767:DeployYoutubeTopic')
     youtube_bucket = s3.Bucket('youtube.rozanski.xyz')
     build_bucket = s3.Bucket('youtube-build.rozanski.xyz')
 
@@ -20,4 +22,7 @@ def lambda_handler(event, context):
             youtube_bucket.upload_fileobj(obj, nm,
                 ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             youtube_bucket.Object(nm).Acl().put(ACL='public-read')
+    print('All done')
+    topic.publish(Subject='Youtube Player deployment complete', Message='Youtube Player deployment complete')
+
     return 'Lambda'
