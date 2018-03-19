9b6f9909d8670e589d91e55058248a81e897c4d3
Armand Rozanski <arozanski@tagworldwide.com>
2018-03-19 14:54:50 +0000

    update deployment script for lambda

--- upload-youtube-player-lambda.py
+++ upload-youtube-player-lambda.py
@@ -4,17 +4,20 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-youtube_bucket = s3.Bucket('youtube.rozanski.xyz')
-build_bucket = s3.Bucket('youtube-build.rozanski.xyz')
+    s3 = boto3.resource('s3')
 
-youtube_zip = io.BytesIO()
-build_bucket.download_fileobj('youtubeplayer.zip', youtube_zip)
+    youtube_bucket = s3.Bucket('youtube.rozanski.xyz')
+    build_bucket = s3.Bucket('youtube-build.rozanski.xyz')
 
-with zipfile.ZipFile(youtube_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        youtube_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        youtube_bucket.Object(nm).Acl().put(ACL='public-read')
+    youtube_zip = io.BytesIO()
+    build_bucket.download_fileobj('youtubeplayer.zip', youtube_zip)
+
+    with zipfile.ZipFile(youtube_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            youtube_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            youtube_bucket.Object(nm).Acl().put(ACL='public-read')
+    return 'Lambda'
