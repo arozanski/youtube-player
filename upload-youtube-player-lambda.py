6da43f038568780700592aa665a8d8c7f6fb9cfb
Armand Rozanski <arozanski@tagworldwide.com>
2018-03-19 15:26:52 +0000

    update deployment script for lambda

--- upload-youtube-player-lambda.py
+++ upload-youtube-player-lambda.py
@@ -8,21 +8,25 @@ def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
     sns = boto3.resource('sns')
-
     topic = sns.Topic('arn:aws:sns:eu-west-2:223963872767:DeployYoutubeTopic')
-    youtube_bucket = s3.Bucket('youtube.rozanski.xyz')
-    build_bucket = s3.Bucket('youtube-build.rozanski.xyz')
 
-    youtube_zip = io.BytesIO()
-    build_bucket.download_fileobj('youtubeplayer.zip', youtube_zip)
+    try:
+        youtube_bucket = s3.Bucket('youtube.rozanski.xyz')
+        build_bucket = s3.Bucket('youtube-build.rozanski.xyz')
 
-    with zipfile.ZipFile(youtube_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            youtube_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            youtube_bucket.Object(nm).Acl().put(ACL='public-read')
-    print('All done')
-    topic.publish(Subject='Youtube Player deployment complete', Message='Youtube Player deployment complete')
+        youtube_zip = io.BytesIO()
+        build_bucket.download_fileobj('youtubeplayer.zip', youtube_zip)
+
+        with zipfile.ZipFile(youtube_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                youtube_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                youtube_bucket.Object(nm).Acl().put(ACL='public-read')
+        print('All done')
+        topic.publish(Subject='Youtube Player deployment complete', Message='Youtube Player deployment complete')
+    except:
+        topic.publish(Subject='Youtube Player deployment failed', Message='Youtube Player deployment failed')
+        raise
 
     return 'Lambda'
